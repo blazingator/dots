e00e34068036df9c19a04f108f598e16aba33b36
Vinicius <vini.domingos69@gmail.com>
2020-09-10 17:07:28 -0300

    Rofi scripts on polybar

---
+++ .config/rofi/scripts/favapps.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+rofi_cmd="rofi -theme $HOME/.config/rofi/scripts/favapps.rasi"
+
+# Options
+firefox=""
+term=""
+sysmon=""
+vim=""
+fm=""
+conf=""
+i3="i3"
+polybar="polybar"
+
+options="$firefox\n$term\n$sysmon\n$vim\n$fm"
+
+chosen="$(echo -e "$options" | $rofi_cmd -p "Apps" -dmenu)"
+case $chosen in
+  $term)
+    lxterminal &
+    ;;
+  $firefox)
+    firefox &
+    ;;
+  $sysmon)
+    lxterminal -e bpytop &
+    ;;
+  $vim)
+    lxterminal -e nvim &
+    ;;
+  $fm)
+    thunar &
+    ;;
+ # $conf)
+  #   lxterminal -e nvim --cmd NERDTree ~/.config
+  #   ;;
+esac
